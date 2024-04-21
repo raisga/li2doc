22d64aa6655ec45416f8da461b374a036ca1d555
jraleman <jraleman@bendiburg.com>
2024-04-20 23:46:03 -0400

    refactor: main python function, adds more deps

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # ======================================================================================================
 # DEPENDENCIES
 
+import asyncio
 import os.path
-from llama_index.core.embeddings import resolve_embed_model
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.ollama import Ollama
+import nest_asyncio
+
+nest_asyncio.apply()
+
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
@@ -12,6 +14,10 @@ from llama_index.core import (
     load_index_from_storage,
     Settings
 )
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+# from openai import AsyncOpenAI
 
 # ======================================================================================================
 # CONSTANTS
@@ -23,9 +29,8 @@ REQUIRED_EXTS = [
     ".pdf",
     ".doc",
     ".docx",
-    # ".xls",
-    # ".xlsx",
-    # ^ ! TEST first before enabling
+    ".xls",
+    ".xlsx",
     ".csv",
     ".html"
 ]
@@ -33,48 +38,96 @@ REQUIRED_EXTS = [
 PERSIST_DIR = "./storage"
 # Model configuration
 MODEL="llama3"
-MODEL_EMBED="BAAI/bge-small-en-v1.5"
+MODEL_EMBED="BAAI/bge-large-en-v1.5"
 REQUEST_TIMEOUT=60.0
-# Query value
-QUERY_VALUE = "Given the data provided, find an operating scissors with a mirror finish, and show me the dimensions of it. Give me the filename and page number of the document where you found the info."
+# Queries values
+SIMILARITY_TOP_K=3
+RESPONSE_MODE="tree_summarize"
+QUERY_PROMPT="Given the data provided, please sumarize the content. Show the summary, and sign with: 🐸"
 
 # ======================================================================================================
+# SCRIPT
+def config_llm() -> None:
+    print("> Configure the LLM")
+    Settings.llm = Ollama(
+        model=MODEL,
+        request_timeout=REQUEST_TIMEOUT
+    )
+    Settings.embed_model = resolve_embed_model(
+        HuggingFaceEmbedding(
+            model_name=MODEL_EMBED
+        )
+    )
+
+    # gets API Key from environment variable OPENAI_API_KEY
+    # client = AsyncOpenAI(
+    #     base_url="http://localhost:3928/v1/",
+    #     api_key="sk-xxx"
+    # )
+    print(f" - model={MODEL}\n"
+        f" - request_timeout={REQUEST_TIMEOUT}\n"
+        f" - model_name={MODEL_EMBED}"
+    )
+
+def load_documents() -> SimpleDirectoryReader:
+    print("> Load documents data")
+    documents = SimpleDirectoryReader(
+        input_dir=INPUT_DIR,
+        required_exts=REQUIRED_EXTS,
+        recursive=True,
+    ).load_data()
+    print(f"input_dir={INPUT_DIR}\n"
+        f"required_exts={REQUIRED_EXTS}"
+    )
+    for d in documents:
+        print(f"file found: {d.metadata["file_name"]}")
+    return documents
+
+def create_index(documents: SimpleDirectoryReader) -> VectorStoreIndex:
+    print("> Check if index storage already exists")
+    if not os.path.exists(PERSIST_DIR):
+        # print("> Load the documents and create the index")
+        # documents = SimpleDirectoryReader(INPUT_DIR).load_data()
+        print("> Creates index storage from documents data chunks")
+        index = VectorStoreIndex.from_documents(documents)
+        print("> Store data to be used for later use")
+        index.storage_context.persist(
+            persist_dir=PERSIST_DIR
+        )
+    else:
+        print("> Load the existing index")
+        storage_context = StorageContext.from_defaults(
+            persist_dir=PERSIST_DIR
+        )
+        index = load_index_from_storage(storage_context)
+    return index
+
 # MAIN
+# -------------------------------------------------------------------------------------------------------
+async def main() -> None:
+    # 1 - Configure model
+    print("\n~~ 🔧 CONFIGURE MODEL ~~")
+    config_llm()
+    # 2 - Load files
+    print("\n~~ 💿 LOAD FILES ~~")
+    documents = load_documents()
+    # 3 - Create index
+    print("\n~~ 📝 CREATE INDEX ~~")
+    index = create_index(documents)
 
-# 1 - Configure model
-Settings.llm = Ollama(model=MODEL, request_timeout=REQUEST_TIMEOUT)
-Settings.embed_model = resolve_embed_model(HuggingFaceEmbedding(
-    model_name=MODEL_EMBED
-))
+    # 4 - Runs query with custom vars
+    print("\n~~ ⌛️ RUN QUERY ~~")
+    print("> Runs query with custom vars")
+    query_engine = index.as_query_engine(
+        similarity_top_k=SIMILARITY_TOP_K,
+        response_mode=RESPONSE_MODE
+    )
 
-# 2 - Load documents data
-documents = SimpleDirectoryReader(
-    input_dir=INPUT_DIR,
-    required_exts=REQUIRED_EXTS,
-    recursive=True,
-).load_data()
+    # 5 - Gets response from query prompt
+    print("\n~~ 💬 GET RESPONSE ~~")
+    print("> Gets response from query prompt")
+    response = query_engine.query(QUERY_PROMPT)
+    print("\n\n\n")
+    print(response)
 
-# 3 - Creates index storage from documents data chunks
-index = VectorStoreIndex.from_documents(
-    documents,
-)
-index.storage_context.persist()
-
-# 4a - Check if index storage already exists
-if not os.path.exists(PERSIST_DIR):
-    # 4a1 - Load the documents and create the index
-    documents = SimpleDirectoryReader(INPUT_DIR).load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    # 4a2 - Store data to be used for later user
-    index.storage_context.persist(persist_dir=PERSIST_DIR)
-else:
-    # 4b - Load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
-
-# 5 - Runs custom query
-query_engine = index.as_query_engine()
-response = query_engine.query(QUERY_VALUE)
-
-# 6 - Prints response
-print(response)
+asyncio.run(main())
